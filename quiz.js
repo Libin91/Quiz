7e444a6bca74f24eb0181076255df43d4b968d3a
Libin91 <airani91@gmail.com>
2020-09-26 18:10:12 +0100

    updated files for quiz

--- quiz.js
+++ quiz.js
@@ -1,71 +1,76 @@
 const myQuestions = [
     {
-        question:'What is the capital city of France?',
-        answers: {
-            a: 'Rome',
-            b: 'Berlin',
-            c: 'Paris',
-     
-
-        },
-        correctAnswer: 'c',
+        qst:'What is the capital city of France?',
+        answerA: 'Rome',
+        answerB: 'Berlin',
+        answerC:'Paris',
+        correctAns: 'Paris'
     },
-        {
-            question: 'What is the capital city of Vietnam?',
-            answers: {
-                a: 'Saigon',
-                b: 'Ho-chi Minh City',
-                c: 'Vientiane',
-            },
-        
-            correctAnswer: 'b',
-        },
+    {
+        qst: 'What is the capital city of Vietnam?',
+        answerA: 'Saigon',
+        answerB: 'Hanoi',
+        answerC: 'Ho-chi Minh City', 
+        correctAns: 'Hanoi'
+    },
 
-             {
-                 question: 'What is the capital city of Saudi Arabia? ',
-                 answers: {
-                    a: 'Riyadh',
-                    b: 'Jeddah',
-                    c: 'Makkah',
-              },
-           correctAnswer: 'a',
-        },
+    {
+        qst: 'What is the capital city of Saudi Arabia? ',
+        answerA: 'Riyadh',
+        answerB: 'Jeddah',
+        answerC: 'Makkah',
+        correctAns: 'Riyadh'
+                     
+              },  
 ];
 
 
+function buildMyQuiz(myQuestions) {
+    const questionsContainer = document.getElementById('questions');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
 
+myQuestions.map((question) => {
+    const readQuestion = document.createElement('p');
+    readQuestion.style.cssText = 'font-weight: bold;';
+    const readQuestionText = document.createTextNode(question.qst);
+    readQuestion.appendChild(readQuestionText);
+    questionsContainer.appendChild(readQuestion);
 
-let i;
-function buildMyQuiz(questionsContainer) {
-   
-    for(i =0; i< myQuestions.length; i++) {
-        let answer= document.createElement('label')
-    };
+    const checkAnswerA = document.createElement('button');
+    const textAnswerA = document.createTextNode(question.answerA);
+    checkAnswerA.appendChild(textAnswerA);
+    questionsContainer.appendChild(checkAnswerA);
+    
 
+    const checkAnswerB = document.createElement('button');
+    const textAnswerB = document.createTextNode(question.answerB);
+    checkAnswerB.appendChild(textAnswerB);
+    questionsContainer.appendChild(checkAnswerB);
 
+    const checkAnswerC = document.createElement('button');
+    const textAnswerC = document.createTextNode(question.answerC);
+    checkAnswerC.appendChild(textAnswerC);
+    questionsContainer.appendChild(checkAnswerC);
 
-
-        
-     
+});
 };
 
-buildMyQuiz()
+function showMyResults(){
+    let correctCount = 0;
+    let userAnswer = button.addEventListener('click');
+myQuestions.map((question) =>{
+    if (userAnswer === question.correctAns) {
+        correctCount++;
 
-
-
-function showMyResults(resultsContainer){
-    let yourAnwer;
-    let rightAnswer = 0;
-    if (yourAnswer === myQuestions[i].correctAnswer){
-        rightAnswer++
-    } else {
 
     }
-};
+
+
+})
+}
 submitButton.addEventListener('click', showMyResults);
 
-const questionsContainer = document.getElementById('questions');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
+
 
 
